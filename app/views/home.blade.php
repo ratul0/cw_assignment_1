2c5345721d7dd16619e5a110367287d05b84a96f
Abu Shahriar Ratul <ratulcse10@gmail.com>
2015-01-21 17:05:34 +0600

    Home Page Update

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,9 @@
 @include('includes.navBar')
 <div class="container">
     <div class="row">
-        This Is Home Page.
+       <h2 align=center> Code Warriors Challange 2015 Web Track (PHP) </h2></br>
+       <h3 align=center> Sample User <a href="{{route('home')}}">Login</a>/<a href="{{route('register')}}">Register</a>/<a href="{{route('logout')}}">Logout</a> Web App </h3></br>
+	   
 
     </div>
 </div>
--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <!--footer start-->
 <footer class="site-footer">
     <div class="text-center">
-        {{Date('Y')}} &copy; SUST CSE
+        {{Date('Y')}} &copy; Shahjalal University of Science and Technology,Sylhet 
 
     </div>
 </footer>
