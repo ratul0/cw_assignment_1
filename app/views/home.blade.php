9fe538a62f19fe691abdbb063604cb882ddc5a78
Abu Shahriar Ratul <ratulcse10@gmail.com>
2015-01-21 17:14:12 +0600

    Homepage fix

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
        <h2 align=center> Code Warriors Challange 2015 Web Track (PHP) </h2></br>
-       <h3 align=center> Sample User <a href="{{route('home')}}">Login</a>/<a href="{{route('register')}}">Register</a>/<a href="{{route('logout')}}">Logout</a> Web App </h3></br>
+       <h3 align=center> Sample User <a href="{{route('login')}}">Login</a>/<a href="{{route('register')}}">Register</a> Web App </h3></br>
 	   
 
     </div>
